7099d51e0d502946c719636af49371857a92c54b
Ghanshyam Verma <ghanshyam.verma@SPI.COM>
2019-09-20 17:40:26 +0530

    new layout

--- app/components/playground.js
+++ app/components/playground.js
@@ -1,46 +1,45 @@
 import React from 'react';
 require('./App.css');
-
-
+import ProductFilter from './playground'
+ 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <Hoc />
+        <ProductFilter />
       </div>
     )
   }
 }
-
+ 
 const withSearch = (WrappedComponent) => {
   return class extends React.Component {
     state = {
       searchTerm: ''
     }
-
+ 
     handleSearch = event => {
       this.setState({ searchTerm: event.target.value })
     }
-
+ 
     render() {
       return (
         <div className="container">
           <div className="input-container">
-            <form className="search-form">
-              <input className="search-bar" ref="search" name="searchBox" onChange={this.handleSearch} value={this.state.searchTerm} type="text" placeholder="Google" />
-            </form>
+            <input className="search-bar" ref="search" name="searchBox" onChange={this.handleSearch} value={this.state.searchTerm} type="text" placeholder="Google" />
           </div>
           <WrappedComponent searchTerm={this.state.searchTerm} />
         </div>
       )
     }
   }
-
+ 
 }
-
-
+ 
+ 
 //filter code
-class CompanyFilter extends React.Component {
+class ProductFilterMenu extends React.Component {
   constructor() {
     super();
     this.handleChange = this.handleChange.bind(this);
@@ -54,70 +53,69 @@ class CompanyFilter extends React.Component {
   
   render() {
     return (
-      <form>
-       <label>Filter: </label>
-        <select id="companyInput" ref="companyInput" onChange={this.handleChange}>
-          <option value=''>All</option>
-          <option value="Keebler">Keebler</option>
-          <option value="Johns">Johns</option>
-          <option value="Yost and Sons">Yost and Sons</option>
+      <form className="filter-menu">
+       <label for="seriesInput">Filter By Series</label>
+        <select id="seriesInput" ref="seriesInput" onChange={this.handleChange}>
+          <option value="All">All</option>
+          <option value="Year-Round Ales">Year-Round Ales</option>
+          <option value="Dictator Series">Dictator Series</option>
+          <option value="Holy Trinity">Holy Trinity</option>
         </select>
       </form>
     );
   }
 }
-
-
-
-
+ 
+//filter ends
+ 
+ 
+ 
+ 
 class Location extends React.Component {
   constructor(){
     super();
-    this.state = { usersData: [], company: '' }
-
+    this.state = { usersData: [], series:0 }
+ 
     this.handleFormInput = this.handleFormInput.bind(this);
   }
   
-
+ 
   componentDidMount() {
     let response = fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
       .then((data) => this.setState({ usersData: data }));
   }
-
-  handleFormInput(company) {
+ 
+  handleFormInput(series) {
     this.setState({
-      company: company
+      series: series
     })
  
-    console.log('company selected : ', company)
+    console.log('Filter options: ', series)
   }
-
+ 
   render() {
+    console.log('Inside Loaction', this.props)
     return (
       <div className="row">
-        <div className="row">
-            <div> <h2 className="heading">Popular</h2> </div>
-            
-            <div className="filter-box">
-              <CompanyFilter
-                  company={this.state.company}
-                  onFormInput={this.handleFormInput}
-                /> 
-            </div>
-        </div>
-
+        <h2 className="heading">Popular</h2>
+ 
+        <ProductFilterMenu
+            series={this.state.series}
+            onFormInput={this.handleFormInput}
+          /> 
+ 
         <div className="flex-container">
           {this.state.usersData
-            .filter(location => `${location.company.name} ${location.address.city} ${location.company.bs}`.toUpperCase().indexOf(this.props.searchTerm.toUpperCase() || this.state.company.toUpperCase() ) >= 0)
+            .filter(location => `${location.company.name} ${location.address.city} ${location.company.bs}`.toUpperCase().indexOf(this.props.searchTerm.toUpperCase()) >= 0)
             .map(location => <LocationCard key={location.id} {...location} />)}
         </div>
-
+ 
       </div>
     )
   }
 }
-
+ 
 const LocationCard = (props) => {
   return (
     <div className="card border-dark mb-3" >
@@ -126,10 +124,11 @@ const LocationCard = (props) => {
         <p className="card-location">{props.address.street}, {props.address.city}</p>
         <p className="card-text">www.{props.website}</p>
         <p className="card-bs">Tags: {props.company.bs}</p>
-
+ 
       </div>
     </div>
   )
 }
-
+ 
 const Hoc = withSearch(Location)
+
